1958aa5cb28f6f6e4207edc6d1313b1373705256
PoisiniouslyZarven <vincent.bozjak@Icloud.com>
2026-03-16 10:51:29 +0100

    Add particles.js for particle effect implementation

---
+++ particles.js
@@ -0,0 +1,30 @@
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 80
+    },
+    color: {
+      value: "#ff8c00"
+    },
+    shape: {
+      type: "circle"
+    },
+    opacity: {
+      value: 0.6
+    },
+    size: {
+      value: 3
+    },
+    move: {
+      enable: true,
+      speed: 2
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#ff8c00",
+      opacity: 0.4,
+      width: 1
+    }
+  }
+});
